742d57f0c9aba0f2e374f402874130f8ca8ab0c9
muscle <muscle@muscles-MacBook-Pro.local>
2016-08-28 22:36:48 -0700

    Add getNumberOfUploads endpoint

--- app/application/controllers/Welcome.php
+++ app/application/controllers/Welcome.php
@@ -23,11 +23,30 @@ class Welcome extends CI_Controller {
 		$this->load->view('welcome_message');
 	}
     
+    // Return number of uploaded pics
+    public function numberOfUploads()
+    {
+        echo $this->getNumberOfUploads();
+    }
+    
+    // Given index in POST, return the number
+    public function getUpload()
+    {
+        
+    }
+    
     public function upload()
     {
         error_log('Uploading photos');
         $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/app/wed-upload/";
-        $target_file = $target_dir . basename($_FILES["files"]["name"][0]);
+        
+        // Stop processing if files is not in the array
+        if (!array_key_exists("files", $_FILES)) {
+            echo "Not an upload.";
+            return;
+        }
+        
+        $target_file = $target_dir . basename(strval($this->getNumberOfUploads()));
         $uploadOk = 1;
         $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
         // Check if image file is a actual image or fake image
@@ -45,14 +64,14 @@ class Welcome extends CI_Controller {
             $uploadOk = 0;
         }
         // Check file size
-        if ($_FILES["files"]["size"][0] > 500000) {
+        if ($_FILES["files"]["size"][0] > 5000000) {
             error_log("Sorry, your file is too large" . (string)$_FILES["files"]["size"][0]);
             $uploadOk = 0;
         }
         // Allow certain file formats
         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" ) {
-            error_log("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+            error_log("Sorry, only JPG, JPEG, PNG & GIF files are allowed: " . $imageFileType);
             $uploadOk = 0;
         }
         // Check if $uploadOk is set to 0 by an error
@@ -66,6 +85,18 @@ class Welcome extends CI_Controller {
                 error_log("Sorry, there was an error uploading your file to: " . $target_file);
                 error_log(print_r($_FILES["files"], true)); // Dump files variable for debugging
             }
+        }
+    }
+    
+    private function getNumberOfUploads()
+    {
+        $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/app/wed-upload/";
+        $contents = scandir($target_dir);
+        if (!$contents) {
+            error_log("Failed to read target directory: " . $target_dir);
+            return 0;
+        } else {
+            return count($contents)-2;
         }
     }
 }
