c9b86fbf93e09f217abefa94b33090e06a911a0e
muscle <muscle@muscles-MacBook-Pro.local>
2016-08-28 20:28:59 -0700

    Change Wed to wed class

--- app/application/controllers/wed.php
+++ app/application/controllers/wed.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Wed extends CI_Controller {
+class wed extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
